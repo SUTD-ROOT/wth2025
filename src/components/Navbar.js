41fd8128def825d69759d20b94fe4629414dd260
VicSyn23 <victoriasynhe@gmail.com>
2025-07-28 09:25:58 +0800

    Added sign up button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,6 +12,7 @@ const Navbar = ({handleScroll}) => {
                     <p onClick={() => handleScroll("about")}>About</p>
                     <p onClick={() => handleScroll("past")}>Past Winners</p>
                     <p onClick={() => handleScroll("faq")}>FAQ</p>
+                    <p className="hide"><a href="https://forms.office.com/r/izxPR7qcrg" target="_blank">Sign Up</a></p>
                     <p onClick={() => handleScroll("contact us")} className="contact-button">Contact Us</p>
                 </div>
             </div>
--- src/components/banner.js
+++ src/components/banner.js
@@ -12,7 +12,7 @@ return (
         <p className="Title2">Dream big, Hack bigger</p>
         <p className="Title3">19 - 21 September 2025</p> 
         <p className="Title4">@Singapore University of Technology and Design</p> 
-        <p className="Title5 hidden"><a href="https://forms.office.com/r/izxPR7qcrg" target="_blank">Sign Up Now!</a></p>
+        <p className="Title5"><a href="https://forms.office.com/r/izxPR7qcrg" target="_blank">Sign Up Now!</a></p>
       </div>
     </div>
   </div>
